ade8235338d6a4e43d9e1f4e67e17e67769700c4
novik-coder <hupengwei2022@163.com>
2024-11-20 16:12:30 +0300

    Delete Lab1-CI-CD/Lab1-CI-CD directory

--- Lab1-CI-CD/Lab1-CI-CD/src/main/java/com/study/lab1cicd/service/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.lab1cicd.service;
-
-import com.study.lab1cicd.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
